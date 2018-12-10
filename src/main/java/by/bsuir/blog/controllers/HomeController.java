c5741c7e49756573285048d72bda93a726c0b9cf
Ilya <roon.77.ilya@gmail.com>
2018-12-10 22:58:41 +0300

    Add controller and html templates

--- src/main/java/by/bsuir/blog/controllers/HomeController.java
+++ src/main/java/by/bsuir/blog/controllers/HomeController.java
@@ -1,7 +1,12 @@
 package by.bsuir.blog.controllers;
 
+import by.bsuir.blog.models.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
@@ -10,15 +15,22 @@ public class HomeController {
         return "index";
     }
 
-    @GetMapping("/post")
-    public String post() {
+    @GetMapping("/post/{id}")
+    public String post(@PathVariable int id, Model model) {
+        Post post = new Post("Test post", "Bla bla bla", "Ilya Runov");
+        model.addAttribute("post", post);
         return "post";
     }
 
     @GetMapping("/about")
     public String about() { return "about"; }
 
-    @GetMapping("/contact")
-    public String contact() { return "contact"; }
+    @GetMapping("/new_post")
+    public String newPost() { return "new_post"; }
 
+    @PostMapping("/post")
+    public String createPost(@ModelAttribute Post post) {
+        System.out.println(post.title);
+        return null;
+    }
 }
